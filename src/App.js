a57c861f38f16aadcb7d9d24870924c93bdadae9
Julius Shapiro <jkurt@Juliuss-MacBook-Pro.local>
2021-01-28 02:18:08 -0600

    redid app with styled-components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,74 @@
 import './App.css';
 import { Route, Switch, Link } from "react-router-dom"
-import { PrivateRoute } from './components/PrivateRoute';
+import { PrivateRoute } from './utils/PrivateRoute';
 import TrainerPage from "./components/trainer/Trainerpage"
+import "./App.css";
+import { useState } from 'react';
+import styled from "styled-components";
+
+
+const PageContainer = styled.div`
+  text-align: center;
+  p {
+    font-family: "Raleway", sans-serif;
+    font-size: 2rem;
+    margin: 5%;
+  }
+  img {
+    margin-top: 25%;
+  }
+  .middle .aligned .row {
+    height: 100%;
+  }
+`;
+
+const StyledLinks = styled.div`
+  .link {
+    color: #B91414;
+  }
+`;
 
 function App() {
-
-  const logout =() => {
+  const [loginStatus, setLoginStatus] = useState(false);
+  const logout = () => {
     localStorage.removeItem("token")
-  }
+    setLoginStatus(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>🏋️‍♀️ ANYWERE FITNESS 🏋️‍♀️</h1>
-      </header>
-      <Route>
-        <div>
-          <ul>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/trainerpage">Trainers</Link>
-            </li>
-            <li>
-              <Link to="/client">Clients</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link onClick={logout}>Logout</Link>
-            </li>
-          </ul>
+      <StyledLinks>
+        <header className="App-header">
+          <h1>🏋️‍♀️ ANYWERE FITNESS 🏋️‍♀️</h1>
+        </header>
+        <div className="ui  menu" style={{ margin: "0" }}>
+          <a className="item link">
+            <Link className="link" to="/dashboard">Dashboard</Link>
+          </a>
+          <a className="link item">
+            <Link className="link" to="/trainerpage">Trainers</Link>
+          </a>
+          <a className="link item">
+            <Link className="link" to="/client">Clients</Link>
+          </a>
+          <a className="link item">
+            <Link className="link" to="/login">Login</Link>
+          </a>
+          {// ternary statement renders logout button only while logged in 
+            (loginStatus)
+              ? <a className="link item"> <Link onClick={logout}>Logout</Link> </a>
+              : <></>
+          }
           <Switch>
             <Route exact path="/login" component={"login placeholder"} />
 
             {/* Private Route instead of route? */}
             <Route exact path="/dashboard" component={"dashboard placeholder"} />
             <PrivateRoute exact path="/trainerpage">
-              <TrainerPage  />
+              <TrainerPage />
             </PrivateRoute>
           </Switch>
         </div>
-      </Route>
+      </StyledLinks>
     </div>
   );
 }
